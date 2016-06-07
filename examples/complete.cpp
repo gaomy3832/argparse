f44e3de0779bf516ee668024e16849f3b521f197
Mingyu Gao <mgao12@stanford.edu>
2016-06-06 21:59:14 -0700

    Renew examples.

--- examples/complete.cpp
+++ examples/complete.cpp
@@ -1,112 +1,97 @@
 #include <iostream>
-#include "argument_parser.h"
+#include "argparse/argument_parser.h"
 
 using namespace argparse;
 
-int main(int argc, char* argv[]) {
+int main(int argc, const char* argv[]) {
     ArgumentParser ap;
 
-    ap.argumentNew(
+    ap.argumentNew<std::string>(
             "a",
-            true,
-            "",
-            {},
             "Positional argument 1.",
-            1,
-            {});
+            1);
 
-    ap.argumentNew(
+    ap.argumentNew<int>(
             "b",
-            true,
-            100,
-            {100, 200, 300},
             "Positional argument 2 with choices from 100, 200, 300.",
             1,
-            {});
+            true,
+            100,
+            {100, 200, 300});
 
-    ap.argumentNew(
+    ap.argumentNew<int>(
             "c",
-            false,
-            10,
-            {},
             "Positional argument 3, optional, and with default.",
             1,
-            {});
+            false,
+            10);
 
     ap.argumentNew(
             "-h",
+            "Print help message.",
+            0,
             false,
             "",
             {},
-            "Print help message.",
-            0,
             {"-help", "--help"});
 
     ap.argumentNew(
             "-f",
-            false,
-            "",
-            {},
             "Simple flag with a very long help description that will be split"
             "automatically into a two column format when usage is printed for"
             "this program. Newlines will also help with justification.\nFor"
             "example:\n0 - an item\n1 - another item\n2 - and another item.",
             0,
+            false,
+            "",
+            {},
             {"-flag", "--flag"});
 
     ap.argumentNew(
             "-l",
+            "A list of arbitrary length.",
+            -1,
             false,
             "",
             {},
-            "A list of arbitrary length.",
-            -1,
             {"-list", "--list"});
 
     ap.argumentNew(
             "-s",
+            "Single string which is required.",
+            1,
             true,
             "",
             {},
-            "Single string which is required.",
-            1,
             {"-str", "--str", "-string", "--string"});
 
     ap.argumentNew(
             "-i",
-            false,
-            1,
-            {1,2,3,4},
             "Integer from 1, 2, 3, 4.",
             2,
-            {});
+            false,
+            1,
+            {1,2,3,4});
 
     ap.argumentNew(
             "-u",
-            false,
-            10,
-            {10u, 20u, 30u, 40u},
             "Unsigned integer from 10, 20, 30, 40.",
             1,
-            {});
-
-    ap.argumentNew(
-            "-float",
             false,
-            "",
-            {},
+            10,
+            {10u, 20u, 30u, 40u});
+
+    ap.argumentNew<float>(
+            "-float",
             "Float number.",
             1,
-            {});
+            false);
 
-    ap.argumentNew(
+    ap.argumentNew<double>(
             "-double",
-            false,
-            "",
-            {},
             "Double number.",
             1,
-            {});
+            false);
 
     ap.cmdlineIs(argc, argv);
 
